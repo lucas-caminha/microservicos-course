59ee129046e30b10db511878c0eaacb66c92195e
lucas-caminha <lucas_santoscaminha@hotmail.com>
2023-02-02 19:27:50 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -6,10 +6,13 @@ import com.devsuperior.hroauth.feignclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -23,6 +26,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email Not Found");
 		}
 		logger.error("Email Found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("Email Not Found: " + username);
+			throw new IllegalArgumentException("Email Not Found");
+		}
+		logger.error("Email Found: " + username);
 		return user;
 	}
 	
